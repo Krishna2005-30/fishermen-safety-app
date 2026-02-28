6cce04c93c4f528a165bbbaba9a0016a01fb66ab
Krishna2005-30 <veni35692@gmail.com>
2026-02-28 08:32:39 +0530

    fix: app.py

--- app.py
+++ app.py
@@ -124,5 +124,6 @@ def nearest_harbor(lat, lon):
 # --------------------
 # RUN APP
 # --------------------
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__": 
+    port = int(os.environ.get("PORT", 5000))  # Render sets this automatically
+    app.run(host="0.0.0.0", port=port, debug=True)
